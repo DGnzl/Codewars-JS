2ed3ec5768dc7480290d2645e14f55d6f4653386
DGnzl <dnlgonzal@outlook.com>
2021-07-08 21:40:09 -0700

    Another commit

--- IsPalindrome.js
+++ IsPalindrome.js
@@ -1,15 +1,15 @@
-// function isPalindrome(str) {
-//     return str.toLowerCase() === str.toLowerCase().split("").reverse().join("")
-// }
+function isPalindrome(str) {
+    return str.toLowerCase() === str.toLowerCase().split("").reverse().join("")
+}
 
-// console.log(isPalindrome("a"))
+console.log(isPalindrome("a"))
 
-// function isPalindrome(str) {
-//     for (let i = 0 ; i < str.length / 2; i++) {
-//         if (str[i].toLowerCase() !== str[str.length - 1 - i].toLowerCase()) {
-//             return false
-//         }
-//     }
-//     return true
-//   }
-// console.log(isPalindrome("aBba"))
+function isPalindrome2(str) {
+    for (let i = 0 ; i < str.length / 2; i++) {
+        if (str[i].toLowerCase() !== str[str.length - 1 - i].toLowerCase()) {
+            return false
+        }
+    }
+    return true
+  }
+console.log(isPalindrome2("aBba"))
