9affb5f61526fca1bab7520d420d53c0c654b350
DGnzl <dnlgonzal@outlook.com>
2021-07-29 18:43:54 -0700

    Domain Parse

---
+++ DomainParse.js
@@ -0,0 +1,20 @@
+function domainParse(str) {
+    let parts = str.split('.')
+    if (parts[0].indexOf('www') != -1) {
+        return parts[1]
+    }
+    else if (parts[0].indexOf('//') != -1) {
+        return parts[0].substring(parts[0].indexOf('/') + 2)
+    }
+    else {
+        return parts[0]
+    }
+}
+
+console.log(domainParse('www.cnet.com'))
+console.log(domainParse('cnet.com'))
+console.log(domainParse('https://cnet.com'))
+
+// function domainName(url){
+// 	return url.replace('http://', '').replace('https://', '').replace('www.', '').split('.').shift()
+// }
