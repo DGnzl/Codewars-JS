eaf540c669365dfc6266ca73023b98a097ec8bd7
DGnzl <dnlgonzal@outlook.com>
2021-08-09 00:29:22 -0700

    Finished kata

--- AverageLength.js
+++ AverageLength.js
@@ -1,3 +1,8 @@
-function averageLength() { 
-    return 0
+function averageLength() {
+  let avg = 0;
+  for (let i = 0; i < input.length; i++) {
+    avg += input[i].length;
   }
+  avg = Math.round(avg / input.length);
+  return input.map((el) => el[0].repeat(avg));
+}
