7c52fb7bf8b44fe00e23a049bbffed6361a116b4
Daniel Gonzalez <dnlgonzal@outlook.com>
2022-03-29 23:39:54 -0700

    additional progress on solution

--- DeafRatsOfHamelin.js
+++ DeafRatsOfHamelin.js
@@ -3,5 +3,7 @@ var countDeafRats = function(town) {
     x.indexOf('P');
     let _temp = x.substring(0, x);
     let _tempLength = _temp.length;
+    let count_left = 0;
+    let count_right = 0;
     return 0;
   }  
