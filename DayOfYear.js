b2bcd42020bf8c640a887de26c5a8404640f7ba9
Daniel Gonzalez <dnlgonzal@outlook.com>
2022-03-30 23:45:42 -0700

    Day of Year solution

---
+++ DayOfYear.js
@@ -0,0 +1,10 @@
+function toDayOfYear(arr) {
+    var now = new Date(`${arr[1]}-${arr[0]}-${arr[2]}`);
+    var start = new Date(now.getFullYear(), 0, 0);
+    var diff = now - start;
+    var oneDay = 1000 * 60 * 60 * 24;
+    var day = Math.floor(diff / oneDay);
+    return day;
+}
+
+console.log(toDayOfYear([25, 12, 2017]))
