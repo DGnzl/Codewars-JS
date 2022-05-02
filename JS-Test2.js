cde9accf09ffff03e0e01b496b820c4df0264713
Daniel Gonzalez <dnlgonzal@outlook.com>
2022-05-01 23:27:27 -0700

    Double Int solution

--- JS-Test2.js
+++ JS-Test2.js
@@ -1,1 +1,11 @@
-console.log(NaN == false)
+function createBase(x) {
+    return function(y) {
+      return x * y;
+    };
+  }
+
+var timesEight = createBase(8); 
+console.log(timesEight(1)); // returns 8
+console.log(timesEight(2)); // returns 16
+console.log(timesEight(3)); // returns 24
+console.log(timesEight(4)); // returns 32
